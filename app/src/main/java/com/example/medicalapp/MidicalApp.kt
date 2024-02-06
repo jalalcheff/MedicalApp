a7b26b2be47f5d3095596e2f7cd0c82ad0f932f0
jalal cheff <jlalalmyahy571@gmail.com>
2024-02-06 23:11:08 +0300

    back to launcher when logged in and go back

--- app/src/main/java/com/example/medicalapp/MidicalApp.kt
+++ app/src/main/java/com/example/medicalapp/MidicalApp.kt
@@ -1,5 +1,7 @@
 package com.example.medicalapp
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
@@ -8,6 +10,7 @@ import com.example.medicalapp.remote.RemoteDatasourceImp
 import com.example.medicalapp.ui.screen.loginScreen.LoginScreen
 import com.example.medicalapp.ui.screen.loginScreen.LoginViewModel
 
+@RequiresApi(Build.VERSION_CODES.O)
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FoodApp(){
--- app/src/main/java/com/example/medicalapp/MidicalNavGraph.kt
+++ app/src/main/java/com/example/medicalapp/MidicalNavGraph.kt
@@ -1,9 +1,12 @@
 package com.example.medicalapp
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.navigation
 import com.example.medicalapp.ui.screen.MainScreen.MainScreenRoute
 import com.example.medicalapp.ui.screen.accountScreen.AccountScreenScreenRoute
 import com.example.medicalapp.ui.screen.addPatientScreen.AddPatient
@@ -12,14 +15,17 @@ import com.example.medicalapp.ui.screen.loginScreen.LoginRoute
 import com.example.medicalapp.ui.screen.loginScreen.LoginScreen
 import com.example.medicalapp.ui.screen.settingsScreen.SettingsScreenRoute
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun MedicalNavGraph(navHostController: NavHostController) {
     NavHost(navController = navHostController, startDestination = "LoginScreen") {
+        navigation(startDestination = "MainScreen", route = "graphOne"){
+            MainScreenRoute(navController = navHostController)
+            AddPatient(navController = navHostController)
+            SettingsScreenRoute(navController = navHostController)
+            AccountScreenScreenRoute(navController = navHostController)
+            DocotrInformationScreenRoute(navController = navHostController)
+        }
         LoginRoute(navController = navHostController)
-        MainScreenRoute(navController = navHostController)
-        AddPatient(navController = navHostController)
-        SettingsScreenRoute(navController = navHostController)
-        AccountScreenScreenRoute(navController = navHostController)
-        DocotrInformationScreenRoute(navController = navHostController)
     }
 }
